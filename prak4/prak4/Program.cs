bc80774ff462c97da24986f2a103fc2d522db1e7
DallasVer <118686739+DallasVer@users.noreply.github.com>
2023-03-04 15:07:14 +0700

    Add files via upload

---
+++ prak4/prak4/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace prak4
+{
+    internal class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Console.Write("Введите римское число: ");
+            string? roman = Console.ReadLine();
+            int result = 0;
+
+            Dictionary<char, int> TranslateRoman = new Dictionary<char, int>
+           {
+                {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}
+           };
+
+            for (int i = 0; i < roman.Length; i++)
+            {
+                if (i != TranslateRoman[] || i + 1 < roman.Length && TranslateRoman[roman[i]] < TranslateRoman[roman[i + 1]])
+                {
+                    result -= TranslateRoman[roman[i]];
+                }
+                else
+                {
+                    result += TranslateRoman[roman[i]];
+                }
+            }
+            Console.WriteLine("Вы ввели: " + result);
+        }
+    }
+}
+    
---
+++ prak4/prak4/Roman.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prak4
+{
+    internal class Roman
+    {
+
+            private static Dictionary<char, int> _romanMap = new Dictionary<char, int>
+           {
+                {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}
+           };
+
+            public static int ConvertRomanToNumber(string text)
+            {
+                int totalValue = 0, prevValue = 0;
+                foreach (var c in text)
+                {
+                    if (!_romanMap.ContainsKey(c))
+                        return 0;
+                    var crtValue = _romanMap[c];
+                    totalValue += crtValue;
+                    if (prevValue != 0 && prevValue < crtValue)
+                    {
+                        if (prevValue == 1 && (crtValue == 5 || crtValue == 10)
+                            || prevValue == 10 && (crtValue == 50 || crtValue == 100)
+                            || prevValue == 100 && (crtValue == 500 || crtValue == 1000))
+                            totalValue -= 2 * prevValue;
+                        else
+                            return 0;
+                    }
+                    prevValue = crtValue;
+                }
+                return totalValue;
+            }
+        }
+}
